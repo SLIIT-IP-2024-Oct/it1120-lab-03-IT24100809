e5701fbf12941658ed2e796f2c47033a4ce6d210
IT24100809 <IT24100809@my.sliit.lk>
2024-11-07 22:56:04 +0530

    Add files via upload
    Q2 completed

---
+++ Q2/IT24100809Lab3Q2.java
@@ -0,0 +1,28 @@
+import java,util.scanner;
+public class IT24100809Lab3Q2{
+public static void main (String[] args){
+
+Scanner scanner = new Scanner(System.in);
+        double monthlySalary;
+        System.out.println("Enter the monthly salary : ");
+	monthlySalary = scanner.nextDouble();
+	System.out.println("monthly salary is : " + monthlySalary);
+	
+	double numberOfOtHours;
+	System.out.println(" Enter the number of OT hours : ");
+	numberOfOtHours = scanner.nextDouble();
+	System.out.println("Number of OT hours : " + numberOfOtHours);
+        
+        double otHoursRate;
+	System.out.println(" Enter OT hours rate : ");
+	otHoursRate = scanner.nextDouble();
+	System.out.println("Number of OT hours : " + otHoursRate);
+        
+        
+        double otAmount = numberOfOtHours * otHoursRate;
+        double totalSalary = monthlySalary + otAmount;
+        
+        System.out.println("total salary including ot is : " + totalSalary);
+
+}
+}
