78889eba7b4e2d7e5d294e962f091bb2fe254164
IT24100809 <IT24100809@my.sliit.lk>
2024-11-08 11:54:39 +0530

    Update IT24100809Lab3Q4.java

--- Q4/IT24100809Lab3Q4.java
+++ Q4/IT24100809Lab3Q4.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class IT24100809Lab3Q4{
     public static void main(String[] args) {
-        int number;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a five-digit number: ");
         int number = input.nextInt();
