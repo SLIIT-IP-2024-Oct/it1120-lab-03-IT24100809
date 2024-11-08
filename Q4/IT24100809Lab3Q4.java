c2ff2c3579c21846b32d0e14946abe2af07cc3b8
IT24100809 <IT24100809@my.sliit.lk>
2024-11-08 11:47:47 +0530

    Update IT24100809Lab3Q4.java

--- Q4/IT24100809Lab3Q4.java
+++ Q4/IT24100809Lab3Q4.java
@@ -6,6 +6,17 @@ public class IT24100809Lab3Q4{
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a five-digit number: ");
         int number = input.nextInt();
+               if (number < 10000 || number > 99999) {
+            System.out.println("Please enter a valid five-digit number.");
+            return;
+        }
+        int digit1 = number / 10000;
+        int digit2 = (number / 1000) % 10;
+        int digit3 = (number / 100) % 10;
+        int digit4 = (number / 10) % 10;
+        int digit5 = number % 10;
+        
+        System.out.println(digit1 + " " + digit2 + " " + digit3 + " " + digit4 + " " + digit5);
 
 
 }
